543aa4cf2c805e72b676bbdf8bc5a880f07bf8a6
Jeremy Raines <jraines@gmail.com>
2016-10-12 16:03:05 -0700

    Product model, seed data, basic layout

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+animals = [{name: "Aardvark",
+            description: "A zero of an animal"},
+            {name: "Bear",
+             description: "A bear for your yard"},
+            {name: "Cat",
+             description: "An extra nice cat"},
+            {name: "Dog",
+             description: "Wolf descendent"},
+            {name: "Emu",
+             description: "Very nice bird"}]
+
+animals.each do |animal|
+  Product.create! animal
+end
