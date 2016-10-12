4a638e37552b18d218939950a6186f3a85627ea0
Jeremy Raines <jraines@gmail.com>
2016-10-12 16:48:10 -0700

    Allow the user to search by id (could still use LIKE but this is more exact)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,7 +9,12 @@ class ProductsController < ApplicationController
     #of where it appears in the text value of the given field
     formatted_query = "%#{params[:query]}%"
 
-    @products = Product.where("name like ? or description like ?", formatted_query, formatted_query)
+    if params[:query].to_i != 0
+      @products = Product.where(id: params[:query])
+    else
+      @products = Product.where("name like ? or description like ?", formatted_query, formatted_query)
+    end
+
     render json: @products
   end
 end
